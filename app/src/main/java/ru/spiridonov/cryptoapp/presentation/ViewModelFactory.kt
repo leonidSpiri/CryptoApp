40e1199438f3b0aa6e99b66c86cd4fafbe737adf
leonidSpiri <2003.leonid2003@gmail.com>
2022-09-06 21:09:04 +0300

    build.gradle

--- app/src/main/java/ru/spiridonov/cryptoapp/presentation/ViewModelFactory.kt
+++ app/src/main/java/ru/spiridonov/cryptoapp/presentation/ViewModelFactory.kt
@@ -5,11 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
+@Suppress("UNCHECKED_CAST")
 class ViewModelFactory @Inject constructor(
     private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return viewModelProviders[modelClass]?.get() as T
     }
 }
